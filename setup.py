1a20e1372a6e4befee86ad32c0a6e316809d5e98
semtim <ofmafowo@gmail.com>
2023-07-11 16:35:02 +0300

    upd setup

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
 from setuptools import setup, find_packages
-from os.path import join, dirname
+
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+		'numpy==1.23.5',
+		'onnxruntime==1.15.1',
+		'pandas==2.0.0',
+		'Requests==2.31.0',
+		'scikit_learn==1.2.2',
+		'setuptools==67.4.0',
+		'skl2onnx==1.14.1',
+		'tqdm==4.65.0',
+		]
 
 setup(
-    name='RBclf',
-    version='0.0',
+    name="RBclf",
+    version="0.0.1",
+    author='Timofey Semenikhin'
+    author_email="ofmafowo@gmail.com",
+    description="Real-bogus classification for ZTF objects.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/snad-space/ztf_real_bogus_clf",
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
 )
